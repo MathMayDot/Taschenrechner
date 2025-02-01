43a9571a99e35facea242a6012365f22d4d2cc60
Matthias <matthiasphung@web.de>
2025-02-01 13:39:20 +0100

    Erster Commit

---
+++ script.js
@@ -0,0 +1,22 @@
+/* JavaScript Datei - script.js */
+"use strict";
+
+function appenOperation(operation) {
+    document.getElementById('resultArea').innerHTML += operation;
+}
+
+function calculateResult(){
+    const calculateResult = document.getElementById('resultArea');
+    const result = eval(calculateResult.innerHTML);
+    calculateResult.innerHTML = result;
+}
+
+function deleteAll(){
+    const deleteAll = document.getElementById('resultArea');
+    deleteAll.innerHTML = '';
+}
+
+function deleteLast(){
+    const deleteLast = document.getElementById('resultArea');
+    deleteLast.innerHTML = deleteLast.innerHTML.slice(0, -1);
+}
